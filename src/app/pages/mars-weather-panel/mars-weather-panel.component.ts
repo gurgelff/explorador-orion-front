a471690975cdedd82d3f74ae02c0958006aa064a
Luis Paulo <luiss_paullo@hotmail.com>
2023-10-30 10:16:33 -0300

    component para logout criado

---
+++ src/app/pages/mars-weather-panel/mars-weather-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mars-weather-panel',
+  templateUrl: './mars-weather-panel.component.html',
+  styleUrls: ['./mars-weather-panel.component.scss']
+})
+export class MarsWeatherPanelComponent {
+
+}
