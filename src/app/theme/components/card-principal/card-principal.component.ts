5b435404da0839ed4a996c7c9f70344f75590fdd
charles <ch4rle2.fh@gmail.com>
2023-11-03 15:36:27 -0300

    Adiciona a biblioteca date-fns e atualiza o componente de cartao de previsao do tempo

--- src/app/theme/components/card-principal/card-principal.component.ts
+++ src/app/theme/components/card-principal/card-principal.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import * as dateFnsLocale from 'date-fns/locale';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-card-principal',
   templateUrl: './card-principal.component.html',
-  styleUrls: ['./card-principal.component.scss']
+  styleUrls: ['./card-principal.component.scss'],
 })
 export class CardPrincipalComponent {
+  IWeatherCard = {
+    temperature: {
+      celsius: {
+        min: -15,
+        max: 31,
+        variation: 5.3,
+      },
+      fahrenheit: {
+        min: 5,
+        max: 86,
+        variation: 5.3,
+      },
+    },
+    terrestrialDate: '2023-09-23T00:00:00Z',
+    solDate: 259,
+  };
 
+  formattedTerrestrialDate: string;
+
+  constructor() {
+    // Converter a data de string para um objeto Date
+    const terrestrialDate = new Date(this.IWeatherCard.terrestrialDate);
+
+    this.formattedTerrestrialDate = format(terrestrialDate, 'dd MMMM yyyy', {
+      locale: dateFnsLocale.ptBR,
+    }).replace(/ /g, ' de ');
+  }
 }
