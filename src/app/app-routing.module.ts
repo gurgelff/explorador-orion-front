bbd6c1f507b17823423ab3ab6f14cf206643bb78
charles <ch4rle2.fh@gmail.com>
2023-11-13 17:59:37 -0300

    fix: Remove o componente do painel de clima de Marte do módulo de roteamento.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
 import { AuthGuardService } from './core/services/auth-guard.service';
-import { MarsWeatherPanelComponent } from './pages/mars-weather-panel/mars-weather-panel.component';
 
 const routes: Routes = [
   {
@@ -17,7 +16,6 @@ const routes: Routes = [
       import('./pages/pages.module').then((m) => m.PagesModule),
     canActivate: [AuthGuardService],
   },
-  { path: 'mars-weather-panel', component: MarsWeatherPanelComponent },
   { path: '**', redirectTo: 'auth' },
 ];
 
